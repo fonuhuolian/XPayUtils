fd4d2a9ac12048cc05c132ce1519f970692d3a33
fonuhuolian <25758058@qq.com>
2019-07-22 09:33:13 +0800

    0.04

--- xpayutils/src/main/java/com/bianmin/xpayutils/AlipayErrorCode.java
+++ xpayutils/src/main/java/com/bianmin/xpayutils/AlipayErrorCode.java
@@ -17,4 +17,20 @@ public enum AlipayErrorCode {
         this.msg = msg;
         this.code = code;
     }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
 }
